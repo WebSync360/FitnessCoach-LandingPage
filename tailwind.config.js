79cf8e7b09aff6e8d5cc4ae984b4422932b28ee1
Stack Quantum <stackquantum1@gmail.com>
2025-12-12 12:02:26 -0800

    Updating UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          50: '#eef2ff',
-          100: '#e0e7ff',
-          500: '#4f46e5',
-          700: '#3730a3'
-          700: '#1e3a2f'
+          50:  '#f3f6f4',       // light silver-white
+          100: '#e9efec',
+          500: '#1e3a2f',       // deep green
+          700: '#14261e',       // deeper green for headings
         },
-        dark: {
-          900: '#0b1220',
-          800: '#0f1724'
-          800: '#f7f7f7' 
-        }
+        neutralbg: '#f7f7f7',   // clean silver-white
       },
       fontFamily: {
-        inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
-      }
-    }
+        inter: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
